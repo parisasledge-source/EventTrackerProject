40960309ef00a9b52195dceec5bcd81f68a653c9
parisasledge-source <parisasledge@me.com>
2022-05-14 01:15:40 -0400

    display for findFlightById created

--- FlightTrackerREST/src/main/webapp/js/eventFunctions.js
+++ FlightTrackerREST/src/main/webapp/js/eventFunctions.js
@@ -13,7 +13,7 @@ function init() {
 	});
 	console.log("in init()");
 	//document.createFlightForm.submit.addEventListener('click', createFlight);
-}	
+}
 
 function getFlight(flightId) {
 	let xhr = new XMLHttpRequest();
@@ -33,6 +33,44 @@ function getFlight(flightId) {
 }
 
 function displayFlight(flight) {
-  let dataDiv = document.getElementById('flightData');
-  dataDiv.textContent = '';
+
+	let dataDiv = document.getElementById('flightData');
+	dataDiv.textContent = '';
+
+	let h2 = document.createElement('h2');
+	h2.textContent = flight.name;
+	dataDiv.appendChild(h2);
+
+	let ul = document.createElement('ul');
+	let fromAirport = document.createElement('li');
+	let toAirport = document.createElement('li');
+	let std = document.createElement('li');
+	let sta = document.createElement('li');
+	let atd = document.createElement('li');
+	let status = document.createElement('li');
+	let airline = document.createElement('li');
+	let aircraft = document.createElement('li');
+	let price = document.createElement('li');
+
+	fromAirport.textContent = flight.fromAirport;
+	toAirport.textContent = flight.toAirport;
+	std.textContent = flight.std;
+	sta.textContent = flight.sta;
+	atd.textContent = flight.atd;
+	status.textContent = flight.status;
+	airline.textContent = flight.airline;
+	aircraft.textContent = flight.aircraft;
+	price.textContent = flight.price;
+
+	ul.appendChild(fromAirport);
+	ul.appendChild(toAirport);
+	ul.appendChild(std);
+	ul.appendChild(sta);
+	ul.appendChild(atd);
+	ul.appendChild(status);
+	ul.appendChild(airline);
+	ul.appendChild(aircraft);
+	ul.appendChild(price);
+
+	dataDiv.appendChild(ul);
 }
