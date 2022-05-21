3c507af3619c131a1d1183e1ff9dcc0c7d184f44
parisasledge-source <parisasledge@me.com>
2022-05-20 23:33:25 -0500

    Website Design-Second Attempt

--- ngFlightTracker/src/app/components/home/home.component.ts
+++ ngFlightTracker/src/app/components/home/home.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  color1 = '#7F6C79';
+  color2 = '#8DAA91';
+  // color3 = '#7F6C79';
+
   subTitle = 'The most amazing flight search engine!';
   //=================
   header = 'Options';
