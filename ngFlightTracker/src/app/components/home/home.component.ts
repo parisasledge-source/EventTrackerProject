6ab463a085f1d8f88128e23435626f8d28d12bd3
parisasledge-source <parisasledge@me.com>
2022-05-20 22:24:42 -0500

    Website Design-First Attempt

--- ngFlightTracker/src/app/components/home/home.component.ts
+++ ngFlightTracker/src/app/components/home/home.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  subTitle = 'The most amazing flight search engine!';
+  //=================
+  header = 'Options';
+  flighInfo = {
+    home: 'Home',
+    about: 'About',
+    search: 'Search'
+  };
+  //===================
   constructor() { }
 
   ngOnInit(): void {
