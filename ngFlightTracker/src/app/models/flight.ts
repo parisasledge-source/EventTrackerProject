b5d220d95038d4ebb17aa0d01115992c2ef3d4ca
parisasledge-source <parisasledge@me.com>
2022-05-21 00:24:32 -0500

    flight.ts entity created

--- ngFlightTracker/src/app/models/flight.ts
+++ ngFlightTracker/src/app/models/flight.ts
@@ -1,2 +1,40 @@
 export class Flight {
+  name: string;
+  std: string;
+  sta: string;
+  atd: string;
+  status: string;
+  fromAirport: string;
+  toAirport: string;
+  airline: string;
+  aircraft: string;
+  price: number;
+  photoUrl: string;
+
+  constructor(
+  name: string ='',
+  std: string ='',
+  sta: string ='',
+  atd: string ='',
+  status: string ='',
+  fromAirport: string ='',
+  toAirport: string ='',
+  airline: string ='',
+  aircraft: string ='',
+  price: number =0,
+  photoUrl: string= ''
+  ){
+    this.name = name;
+    this.std = std;
+    this.sta = sta;
+    this.atd = atd;
+    this.status = status;
+    this.fromAirport = fromAirport;
+    this.toAirport = toAirport;
+    this.airline = airline;
+    this.aircraft = aircraft;
+    this.price = price;
+    this.photoUrl = photoUrl;
+
+  }
 }
